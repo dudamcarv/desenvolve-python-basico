ddf90c18f82dbdfae9c3b89ff264049779eafb1a
dudamcarv <163474967+dudamcarv@users.noreply.github.com>
2025-03-17 22:33:27 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,10 @@
+var1 = 5
+var2 = 2
+
+print("Tipo de var1:", type (var1))
+print("Tipo de var2:", type (var2))
+
+var3 = var1 / var2
+
+print("Valor de var3:", var3)
+print("Tipo de var3:", type(var3))
